a9c091ff4d6e79d26425ca574c84a6384886ad12
djdcann <djdcann@gmail.com>
2018-04-05 08:29:35 -0400

    ECC Diffie Hellman

--- src/src/ECDH.java
+++ src/src/ECDH.java
@@ -1,5 +1,6 @@
 import wit.security.ecc.*;
 import java.math.*;
+import java.util.Random;
 
 public class ECDH {
 
@@ -19,65 +20,30 @@ public class ECDH {
 
         EllipticCurve cc = new EllipticCurve(new BigInteger("11"), new BigInteger("1"), new BigInteger("6"));
         Point P = new Point(cc, new BigInteger("2"), new BigInteger("7"));
-        Point Q = new Point(cc, new BigInteger("3"), new BigInteger("6"));
+        int n = numberOfPoints(P);
+        Random rand = new Random();
 
         System.out.println(cc);
+        System.out.println("Starting point P: " + P);
+        System.out.println("Number of points in the curve: " + n);
 
-        System.out.println("EXACT NUMBER OF POINTS: " + numberOfPoints(P));
+        BigInteger XA = new BigInteger(Integer.toString(rand.nextInt(n)));
+        Point PA = P.multiply(XA);
 
-        System.out.println("1P: " + P);
+        System.out.println("User A selects a random private key: " + XA);
+        System.out.println("User A calculates point PA: " + PA);
 
-        Point P2 = P.add(P);
-        System.out.println("2P: " + P2);
+        BigInteger XB = new BigInteger(Integer.toString(rand.nextInt(n)));
+        Point PB = P.multiply(XB);
 
-        Point P4 = P2.add(P2);
-        System.out.println("4P: " + P4);
+        System.out.println("User B selects a random private key: " + XB);
+        System.out.println("User B calculates point PB: " + PB);
 
-        Point P5 = P4.add(P);
-        System.out.println("5P: " + P5);
-
-        Point P9 = P5.add(P4);
-        System.out.println("9P: " + P9);
-
-        Point P18 = P9.add(P9);
-        System.out.println("18P: " + P18);
-
-        Point M5 = P.multiply(new BigInteger("5"));
-        System.out.println("5M: " + M5);
-
-        Point M4 = P.multiply(new BigInteger("4"));
-        System.out.println("4M: " + M4);
-
-        Point p_plus_q = P.add(Q);
-        System.out.println("P+Q: " + p_plus_q);
-
-        System.out.println("\n--- Points from 1 to 20 ---");
-        for(int i=1; i <= 20; i++) {
-            Point t = P.multiply(new BigInteger(i+""));
-            System.out.println(i+"*P= "+ t);
-        }
-        System.out.println();
-
-        Point Pi = new Point(cc);
-        System.out.println("oo + oo= " + Pi.add(Pi));
-        System.out.println("oo + P = " + Pi.add(P));
-        System.out.println("P  + oo= " + P.add(Pi));
-        System.out.println("oo * 3 = " + Pi.multiply(new BigInteger(3+"")));
-
-        Point a1 = Pi.subtract(P5);
-        Point a2 = a1.add(P5);
-        System.out.println();
-        System.out.println("oo - P5:      " + a1);
-        System.out.println("oo - P5 + P5: " + a2);
-        System.out.println();
-
-        System.out.println("oo + oo: " + Pi.add(Pi));
-        System.out.println("oo - oo: " + Pi.subtract(Pi));
-        System.out.println();
-        System.out.println("-----------");
-        Point B1 = new Point(cc, new BigInteger("5"), new BigInteger("9"));
-        Point B2 = new Point(cc, new BigInteger("5"), new BigInteger("2"));
-        System.out.println("(5,9) - (5,2): " + B1.subtract(B2));
-        System.out.println("Q+(-Q): " + Q.subtract(Q));
+        // XB and XA are exchanged between users
+        System.out.println("User A and B exchange the calculated points.");
+        Point KA = PB.multiply(XA);
+        System.out.println("User A calculates point KA: " + KA);
+        Point KB = PA.multiply(XB);
+        System.out.println("User B calculates point KB: " + KB);
     }
 }
